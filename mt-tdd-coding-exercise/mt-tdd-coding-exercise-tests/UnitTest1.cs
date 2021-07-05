9c875c7cc11f1eeeca61040bb2b084af06afef4b
Gareth Pitt-Nash <gareth@pitt-nash.com>
2021-07-05 15:20:55 +0100

    Initial revision (blank projects)

---
+++ mt-tdd-coding-exercise/mt-tdd-coding-exercise-tests/UnitTest1.cs
@@ -0,0 +1,18 @@
+using NUnit.Framework;
+
+namespace mt_tdd_coding_exercise_tests
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
---
+++ mt-tdd-coding-exercise/mt-tdd-coding-exercise/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace mt_tdd_coding_exercise
+{
+    public class Class1
+    {
+    }
+}
